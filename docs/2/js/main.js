4b70b9bda9a04231a5fed33cbbbec23a9b5994e6
ytyaru <yttry0@gmail.com>
2025-07-08 17:06:15 +0900

    add 2

---
+++ docs/2/js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const author = 'ytyaru';
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/JS.InputRangeNumber.20250708135436/`}, 'InputRangeNumber')),
+        van.tags.p('input要素のrangeとnumberを組合せたWebComponentを作る。'),
+//        van.tags.p('Create a WebComponent that combines the range and number of input elements.'),
+    );
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make());
+
+    const a = new Assertion();
+    a.t(()=>{
+        const one = document.querySelector(`#one`);
+        const two = document.querySelector(`#two`);
+        return one!==two && [one, two].every(el=>el instanceof HTMLInputRangeNumberElement);
+    });
+    a.t(()=>{
+        const one = document.querySelector(`#one`);
+        const two = document.querySelector(`#two`);
+        console.log(one.value, two.value, typeof one.value);
+        return '0'===one.value && '0'===two.value;
+    });
+    a.t(()=>{
+        const one = document.querySelector(`#one`);
+        const two = document.querySelector(`#two`);
+        return '0'===one.min && '0'===two.min;
+    });
+
+
+    console.log(document.querySelector(`#one`))
+    console.log(document.querySelector(`#two`));
+    document.querySelector(`#two`).value = 999;
+    console.log(document.querySelector(`#two`).value);
+    document.querySelector(`#two`).value = 30;
+    console.log(document.querySelector(`#two`).value);
+    a.t(true);
+    a.f(false);
+    a.e(TypeError, `msg`, ()=>{throw new TypeError(`msg`)});
+    a.fin();
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
---
+++ docs/2/js/range-number.js
@@ -0,0 +1,128 @@
+class HTMLInputRangeNumberElement extends HTMLElement {
+    static get observedAttributes() {return 'value min max step'.split(' ')}
+    constructor() {
+        super();
+        //this._ = {value:0, min:0, max:100, step:1, el:{}, handler:{}};
+        this._ = {d:{s:{}, n:{}}, el:{}, handler:{}};
+        this._.d.n = {value:0, min:0, max:100, step:1};
+        this._.d.s = {value:'0', min:'0', max:'100', step:'1'};
+        this._.el.number = null;
+        this._.el.range = null;
+        this._.el.host = null;
+        this._.handler.number = {input:null, change:null};
+        this._.handler.range = {input:null, change:null};
+        this._.handler.host = {input:null, change:null};
+        console.log(this._)
+        this.#makeEl();
+    }
+    connectedCallback() {
+        this.append(this._.el.host);
+        this.#resizeNumber()
+    }
+    disconnectedCallback() {
+        'number range host'.map(t=>{
+            'input change'.map(e=>this._.el[t].removeEventListener(e, this._.handler[t][e]));
+        })
+    }
+    get value() {return this._.el.number.value}
+    set value(v) {
+        //if ('number'!==typeof v) {return}
+        if ('number'!==typeof v) {v=parseFloat(v)}
+        if (v < parseFloat(this._.min) || parseFloat(this._.max) < v) {return}
+        'number range'.split(' ').map(n=>this._.el[n].value=v)
+    }
+    //get min() {return this._.d.s.min}
+    get min() {return this._.el.number.min}
+    set min(v) {this.#set('min')}
+    //get max() {return this._.d.s.max}
+    get max() {return this._.el.number.max}
+    set max(v) {this.#set('max')}
+    //get step() {return this._.d.s.step}
+    get step() {return this._.el.number.step}
+    set step(v) {this.#set('step')}
+    #set(t) {
+        //if (!this.#setN(v) && !this.#setS(v)) {throw new TypeError('NumberかString型のみ有効です。')}
+        if (this.#setN(v) || this.#setS(v)) {'number range'.split(' ').map(n=>this._.el[n].value=v); return true}
+        else {return false}
+        /*
+        if ('number'===typeof v && !Number.isNaN(v) && v!==this._.d.n[t]) {
+            this._.d.n[t] = v;
+            this._.d.s[t] = v.toString();
+        } else if ('string'===typeof v || v instanceof String && v!==this._.d.s[t]) {
+            this._.d.s[t] = v;
+            this._.d.n[t] = Number(v);
+        }
+        */
+    }
+    #setN() {
+        if ('number'===typeof v && !Number.isNaN(v)) {// && v!==this._.d.n[t]
+            this._.d.n[t] = v;
+            this._.d.s[t] = v.toString();
+            return true
+        } else {return false}
+    }
+    #setS() {
+        if ('string'===typeof v || v instanceof String) {// && v!==this._.d.s[t]
+            this._.d.s[t] = v;
+            this._.d.n[t] = Number(v);
+            return true
+        } else {return false}
+    }
+    #isValid(v){}
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        this[property] = newValue;
+
+    }
+    #makeEl() {
+        this._.el.number = this.#makeInput('number');
+        this._.el.range = this.#makeInput('range');
+        this._.el.host = this.#makeDiv();
+        this._.el.host.append(this._.el.number, this._.el.range);
+        this._.handler.host.input = (e)=>{
+            e.preventDefault();
+            if (!(e instanceof CustomEvent)) {return}
+            console.log(e)
+//            if (this._.el.host===e.detail.sender){return}
+            for (let el of [this._.el.number, this._.el.range].filter(el=>el!==e.detail.sender)) {
+                el.value = e.detail.value;
+            }
+            this._.d.s.value = e.detail.value.toString();
+            this._.d.n.value = Number(e.detail.value.toString());
+        };
+        this._.handler.host.change = this._.handler.host.input;
+        'input change'.split(' ').map(type=>this._.el.host.addEventListener(type, this._.handler.host[type]))
+    }
+    #dispatch(type, sender, value) {
+        this._.el.host.dispatchEvent(new CustomEvent(type, {detail:{sender:sender, value:value}}));
+    }
+    #makeDiv() {
+        const div = document.createElement('div');
+        div.style.display = 'flex';
+        div.style.flexDirection = 'row';
+        div.style.flexWrap= 'wrap'; // nowrap/wrap/wrap-reverse
+        //div.style.justifyContent = 'center';//align-items
+        div.style.alignItems = 'center';//align-items
+        return div;
+    }
+    #makeInput(type) {
+        const input = document.createElement('input');
+        input.type = type;
+        //'value min max step'.split(' ').map(k=>input[k]=this._[k]);
+        'value min max step'.split(' ').map(k=>input[k]=this._.d.s[k]);
+        for (let evNm of 'input change'.split(' ')) {
+            console.log(type, evNm, this._.handler)
+            this._.handler[type][evNm]=(e)=>{e.preventDefault();this.#dispatch(e.type, e.currentTarget, e.currentTarget.value);};
+            input.addEventListener(evNm, this._.handler[type][evNm]);
+        }
+        return input;
+    }
+    #resizeNumber() {
+        console.log(getComputedStyle(this._.el.number).fontSize)
+        console.log(this._.el.number.style)
+        console.log(this._.el.number.style.width)
+        //this._.el.number.style.width = `${parseFloat(getComputedStyle(this._.el.number).fontSize) * this._.max.toString().length}px`;
+        this._.el.number.style.width = `${parseFloat(getComputedStyle(this._.el.number).fontSize) * this._.d.s.max.length}px`;
+    }
+}
+customElements.define('range-number', HTMLInputRangeNumberElement);
